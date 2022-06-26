4a6a40c961513be148fcf853681c1a3a4bab00c5
DiChag <104024866+DiChag@users.noreply.github.com>
2022-06-26 11:46:39 +0000

    Deploying to gh-pages from @ DiChag/icecream-and-beer@12d302d99a2a884ea7cecdd343c38f36250b17d8 🚀

---
+++ index.a823e330.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.a823e330.js.map
---
+++ index.c1066750.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.c1066750.js.map
