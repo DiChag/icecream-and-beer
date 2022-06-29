1f72b64c76f6f6562540730031ebd1ff85088fe8
KravchuK25 <Krauch.d25@gmail.com>
2022-06-29 23:42:56 +0300

    gotovo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './js/compound__cream';
 import './js/compound__coffee';
 import './js/compound__milkshakes';
 import './js/map-close-open';
-import './js/button-close-open';
+
 import './js/menu';
 import './js/modal';
 import './js/map-close-open';
